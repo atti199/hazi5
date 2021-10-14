228325c5a1998eeedb6f33dbc13330e94983cb5f
atti199 <atti199@gmail.com>
2021-10-14 16:14:05 +0200

    init

--- Untitled.R
+++ Untitled.R
@@ -1,6 +1,12 @@
 library(gapminder)
 library(tidyverse)
 library(ggplot2)
+# git add .
+# git commit -m "init"
+# git push
+
+# 1. feladat --------------------------------------------------------------
+
 
 gapminder <- gapminder
 gap_years  <- gapminder %>% 
@@ -11,4 +17,13 @@ evek <- unique(gapminder$year)
 files <- str_c("gapminder", evek)
 map2(gap_years[[2]], files, write_csv)
 
-cor(log(gapminder$gdpPercap), gapminder$lifeExp)
+gap_years$korrelacio <- cor(log(gap_years[[2]]$gdpPercap), gap_years[[2]]$lifeExp)
+
+for (i in 1:12) {
+  gap_years[["korrelacio"]][i] <-cor(log(gap_years[[2]][[i]]["gdpPercap"]), gap_years[[2]][[i]]["lifeExp"])
+}
+
+
+# 2.feladat ---------------------------------------------------------------
+
+
